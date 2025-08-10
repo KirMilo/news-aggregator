2e373d7c7b2a2426444832758b2c668dfafed65d
KirMilo <kerill.milovanov2@mail.ru>
2025-08-10 20:30:20 +0300

    db-service init

---
+++ db-service/db_service/api/v1/news/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class News(models.Model):  # Новости
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    time_create = models.DateTimeField(auto_now_add=True)
+
+    categories = models.ManyToManyField("NewsCategory", related_name='news_categories')
+    source = models.ForeignKey("Source", on_delete=models.SET_NULL, related_name='news_sources', null=True)
+
+
+class Category(models.Model):  # Категории
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class NewsCategory(models.Model):  # Ассоциативная таблица новости-категории
+    news_id = models.ForeignKey("News", on_delete=models.CASCADE, related_name='news_news_id')
+    category = models.ForeignKey("Category", on_delete=models.SET_NULL, related_name='news_category_id', null=True)
+
+
+class NewsImages(models.Model):  # Прикрепленные к новости изображения
+    url = models.URLField()
+    news = models.ForeignKey(News, on_delete=models.SET_NULL, related_name='news_images', null=True)
+
+
+class Source(models.Model):
+    link = models.URLField()
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.link
