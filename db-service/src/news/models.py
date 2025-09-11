898a99e9ccc8a3cff06f3136b5ddd06c70f91f29
KirMilo <kerill.milovanov2@mail.ru>
2025-09-11 16:08:22 +0300

    db-service: Добавлена админка News. Добавлен сигнал на сохранение, обновление, удаление записей для обновления в elasticsearch

--- db-service/src/news/models.py
+++ db-service/src/news/models.py
@@ -2,16 +2,30 @@ from django.db import models
 
 
 class News(models.Model):  # Новости
-    title = models.CharField(max_length=255)
-    body = models.TextField()
-    published_at = models.DateTimeField(auto_now_add=True)
-    link = models.URLField()
-    active = models.BooleanField(default=True)
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    body = models.TextField(verbose_name="Статья")
+    published_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата публикации")
+    link = models.URLField(verbose_name="Ссылка")
+    active = models.BooleanField(default=True, verbose_name="Опубликовано")
 
-    source = models.ForeignKey("Source", on_delete=models.SET_NULL, related_name='news', null=True)
+    source = models.ForeignKey(
+        "Source",
+        on_delete=models.SET_NULL,
+        related_name='news',
+        null=True,
+        verbose_name="Источник",
+    )
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = "Новости"
+        verbose_name_plural = verbose_name
+        ordering = ["-published_at"]
+        indexes = [
+            models.Index(fields=["-published_at"])
+        ]
 
 
 class Source(models.Model):
--- db-service/src/news/signals.py
+++ db-service/src/news/signals.py
@@ -1,15 +1,9 @@
-# from django.db.models.signals import post_save, post_delete
-# from django.dispatch import receiver
-# from django_elasticsearch_dsl.registries import registry
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django_elasticsearch_dsl.registries import registry
+from news.models import News
 
 
-# @receiver((post_save, post_delete))
-# def on_update_document(sender, **kwargs):
-#     app_label = sender._meta.app_label
-#     model_name = sender._meta.model_name
-#     instance = kwargs['instance']
-#     if app_label == 'news':
-#         if model_name == 'news':
-#             instances = instance.news.all()
-#             for _instance in instances:
-#                 registry.update(_instance)
+@receiver(signal=(post_save, post_delete), sender=News)
+def on_update_document(**kwargs):  # noqa
+    registry.update(News)
